34643240110bf35189a0698467449604e218de8a
志聰 林 <ed100084@edah.org.tw>
2015-12-04 09:49:14 +0800

    c2v model

---
+++ WebApplication6/Controllers/VtoCController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication6.Controllers
+{
+    public class VtoCController : Controller
+    {
+        // GET: VtoC
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult DemoQueryString()
+        {
+            ViewBag.id = int.Parse(Request.QueryString["id"]);
+            return View();
+        }
+
+        public ActionResult DemoRouteData(int id)
+        {
+            ViewBag.id = id;
+            return View();
+        }
+
+        public ActionResult BasicModelBinding(string name)
+        {
+            ViewBag.Name = name;
+            return View();
+        }
+
+        public ActionResult BasicModelBingingByModel(string name)
+        {
+            ViewBag.Model = name;
+            return View(); 
+        }
+    }
+}
